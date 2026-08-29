4e630024beb2993d7e3ad81759cca58ee85f6ee0
abhigyanranjan71-cmd <abhigyanranjan71@gmail.com>
2026-08-29 23:07:20 +0530

    Add files via upload

---
+++ frontend/build_frontend.py
@@ -0,0 +1,9 @@
+# QuantumShield Frontend Generator
+import os
+
+os.makedirs('static/css', exist_ok=True)
+os.makedirs('static/js', exist_ok=True)
+os.makedirs('static/images', exist_ok=True)
+os.makedirs('templates/partials', exist_ok=True)
+
+print('Directories ready')
