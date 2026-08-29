cba51ce7c5f80e2914cdb19dd5d06351662bdf71
abhigyanranjan71-cmd <abhigyanranjan71@gmail.com>
2026-08-29 23:07:39 +0530

    Delete build_frontend.py

--- build_frontend.py
+++
@@ -1,9 +0,0 @@
-# QuantumShield Frontend Generator
-import os
-
-os.makedirs('static/css', exist_ok=True)
-os.makedirs('static/js', exist_ok=True)
-os.makedirs('static/images', exist_ok=True)
-os.makedirs('templates/partials', exist_ok=True)
-
-print('Directories ready')
